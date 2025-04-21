adf2baf082f49e5977367b9dcf634c99c38ab0f0
Abel Lukwaro <abelmabruke@gmail.com>
2025-04-21 20:03:33 +0300

    Initial Commit

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({ plant, onDeletePlant }) {
+  const [isSoldOut, setIsSoldOut] = useState(false);
+
+  function handleSoldOutClick() {
+    setIsSoldOut((prev) => !prev);
+  }
+
+  function handleDeleteClick() {
+    fetch(`http://localhost:6001/plants/${plant.id}`, {
+      method: "DELETE",
+    }).then(() => onDeletePlant(plant.id));
+  }
+
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: {plant.price}</p>
+      <button className={isSoldOut ? "" : "primary"} onClick={handleSoldOutClick}>
+        {isSoldOut ? "Out of Stock" : "In Stock"}
+      </button>
+      <button onClick={handleDeleteClick}>Delete</button>
     </li>
   );
 }
